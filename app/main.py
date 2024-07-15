2e159ee5ee9aebea73cfc56ac95fac35ad51c0d4
toriningen <toriningen.me@gmail.com>
2024-07-15 08:39:38 +0300

    wip

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from .tables import *
 from .text_util import split_long_text
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.getLevelName(LOG_LEVEL),
     format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
@@ -39,6 +39,7 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 
     @bot.on(events.NewMessage(incoming=True))
     async def on_new_message(event):
+        logger.debug(f'New message', extra={"event": event.to_dict()})
         orig_text = event.raw_text
         glag_text = to_glag(orig_text)
 
@@ -49,20 +50,21 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 
     @bot.on(events.InlineQuery())
     async def inline_handler(event):
+        logger.debug(f'Inline query', extra={"event": event.to_dict()})
         orig_text = event.text
         glag_text = to_glag(orig_text)
 
         if not glag_text:
             return await event.answer([])
 
-        hint = ''
-        if len(orig_text) == 255:
-            hint = '(⚠️ можливо, текст занадто довгий - є обмеження в 270 знаків) '
+        hint = 'надіслати транслітерацію в чат'
+        if len(orig_text) >= 255:
+            hint = '⚠️ можливо, текст занадто довгий!'
 
         return await event.answer([
             event.builder.article(
                 title=f'({len(orig_text)} | {len(glag_text)}) {ellipsis_truncate(glag_text, 100)}',
-                description=f"{hint}надіслати транслітерацію в чат",
+                description=f"{hint}",
                 text=glag_text,
             )
         ])
