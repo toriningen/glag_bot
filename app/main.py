13bb6058437fff699f1b1a4af95e4f9808483a5a
toriningen <toriningen.me@gmail.com>
2024-07-15 08:47:24 +0300

    wip

--- app/main.py
+++ app/main.py
@@ -37,12 +37,15 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 
     @bot.on(events.NewMessage(incoming=True, pattern=r'^/start'))
     async def on_start(event):
+        logger.debug(f'Start message', extra={"event": event.to_dict()})
+
         await event.reply(to_glag('Добродошли!'))
         raise events.StopPropagation()
 
     @bot.on(events.NewMessage(incoming=True))
     async def on_new_message(event):
-        logger.info(f'New message', extra={"event": event.to_dict()})
+        logger.debug(f'New message', extra={"event": event.to_dict()})
+
         orig_text = event.raw_text
         glag_text = to_glag(orig_text)
 
@@ -53,7 +56,8 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 
     @bot.on(events.InlineQuery())
     async def inline_handler(event):
-        logger.info(f'Inline query', extra={"event": event.to_dict()})
+        logger.debug(f'Inline query', extra={"event": event.to_dict()})
+        
         orig_text = event.text
         glag_text = to_glag(orig_text)
 
