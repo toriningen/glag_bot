2767403f6e194cf223b10fe3e3833641f10eff3a
toriningen <toriningen.me@gmail.com>
2024-07-15 11:42:46 +0300

    wip

--- app/converter.py
+++ app/converter.py
@@ -1,9 +1,26 @@
 import re
-from typing import List
+from typing import List, Mapping, Callable, Tuple, Set
 
 
-def build_converter(tables: List[str]):
-    lines = [line for table in tables for line in table.splitlines()]
+def _make_matcher(patterns: List[str]) -> re.Pattern[str]:
+    return re.compile('|'.join(re.escape(pat) for pat in patterns))
+
+
+def _ruleset_to_converter(rules: List[Tuple[str, str]]) -> Callable[[str], str]:
+    # longer replacements come first, relative order is preserved
+    rules.sort(key=lambda x: -len(x[0]))
+    rulemap = dict(rules)
+
+    rx = _make_matcher([left for left, _ in rules])
+
+    def replace(text: str):
+        return rx.sub(lambda m: rulemap.get(m.group(0), m.group(0)), text)
+
+    return replace
+
+
+def _table_to_ruleset(table: str) -> List[Tuple[str, str]]:
+    lines = table.splitlines()
     rules = []
 
     for raw in lines:
@@ -29,13 +46,54 @@ def build_converter(tables: List[str]):
         for l, r in zip(left, right):
             rules.append((l, r))
 
-    # longer replacements come first, relative order is preserved
-    rules.sort(key=lambda x: -len(x[0]))
-    rulemap = dict(rules)
+    return rules
 
-    rx = re.compile('|'.join(re.escape(left) for left, _ in rules))
 
-    def replace(text: str):
-        return rx.sub(lambda m: rulemap.get(m.group(0), m.group(0)), text)
+def _get_unique_rules(rulesets: Mapping[str, List[Tuple[str, str]]]) -> Mapping[str, Set[str]]:
+    rulesets = {lang: dict(rules) for lang, rules in rulesets.items()}
 
-    return replace
+    # identify left rules that are unique to given lang code
+    uniques = {}
+    for lang1, ruleset1 in rulesets.items():
+        union = set()
+
+        for lang2, ruleset2 in rulesets.items():
+            if lang1 == lang2:
+                continue
+            union |= ruleset2.keys()
+
+        uniques[lang1] = ruleset1.keys() - union
+        assert uniques[lang1]
+
+    return uniques
+
+
+def _get_unique_detectors(uniques: Mapping[str, Set[str]]) -> Mapping[str, Callable[[str], bool]]:
+    detectors = {}
+
+    for lang, unique in uniques.items():
+        rx = _make_matcher(sorted(unique, key=lambda x: -len(x[0])))
+        detectors[lang] = lambda text: rx.search(text) is not None
+
+    return detectors
+
+
+class Converter:
+    def __init__(self, tables: Mapping[str, str]):
+        self.tables = tables
+        self.rulesets = {lang: _table_to_ruleset(table) for lang, table in tables.items()}
+        self.converters = {lang: _ruleset_to_converter(ruleset) for lang, ruleset in self.rulesets.items()}
+        self.uniques = _get_unique_rules(self.rulesets)
+        self.unique_detectors = _get_unique_detectors(self.uniques)
+
+    def convert(self, lang: str, text: str) -> str:
+        return self.converters[lang](text)
+
+    def detect_lang(self, text: str) -> Set[str]:
+        detected = set()
+
+        for lang, detect in self.unique_detectors.items():
+            if detect(text):
+                detected.add(lang)
+
+        return detected
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from typing import List
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, Button
 
 from .config import *
-from .converter import build_converter
+from .converter import Converter
 from .logging import get_logger
 from .tables import *
 from .text_util import split_long_text
@@ -18,20 +18,31 @@ def ellipsis_truncate(text: str, max_len: int) -> str:
 
     return text
 
+
 def log_event(event):
     return {
         'event': event.to_json(ensure_ascii=False),
     }
 
+
 def make_bot(session: str, tables: List[str]) -> TelegramClient:
-    to_glag = build_converter(tables)
+    converter = Converter({'cyr': UKR_TABLE, 'isv': ISV_TABLE})
+    from_cyr = lambda text: converter.convert('cyr', text)
+    from_isv = lambda text: converter.convert('isv', text)
+
     bot = TelegramClient(session, api_id=API_ID, api_hash=API_HASH, catch_up=True)
+
+    async def reply_long(event, text):
+        for chunk in split_long_text(text):
+            chunk = chunk.strip()
+            if chunk:
+                await event.reply(chunk)
 
     @bot.on(events.NewMessage(incoming=True, pattern=r'^/start'))
     async def on_start(event):
         logger.debug(f'Start message', extra=log_event(event))
 
-        await event.reply(to_glag('Добродошли!'))
+        await event.reply(f'{from_cyr('Вітаю!')} {from_isv('Добродошли!')}')
         raise events.StopPropagation()
 
     @bot.on(events.NewMessage(incoming=True))
@@ -39,34 +50,62 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
         logger.debug(f'New message', extra=log_event(event))
 
         orig_text = event.raw_text
-        glag_text = to_glag(orig_text)
+        langs = converter.detect(orig_text)
+        if len(langs) == 1:
+            lang, = langs
+            glag_text = converter.convert(lang, orig_text)
+            return await reply_long(event, glag_text)
+        else:
+            candidates = set([converter.convert(lang, orig_text) for lang in langs])
+            if len(candidates) == 1:
+                glag_text = candidates.pop()
+                return await reply_long(event, glag_text)
+            else:
+                await event.reply("⚠️ Яка це мова?", buttons=[
+                    [Button.inline("українська", 'ukr'), Button.inline("міжслов'янська", 'isv')]
+                ])
 
-        for chunk in split_long_text(glag_text):
-            chunk = chunk.strip()
-            if chunk:
-                await event.reply(chunk)
+    @bot.on(events.CallbackQuery())
+    async def on_button(event):
+        data = event.data.decode('utf-8')
+        if data == 'ukr':
+            await event.answer('ukr clicked!')
+        elif data == 'isv':
+            await event.answer('isv clicked!')
 
     @bot.on(events.InlineQuery())
     async def inline_handler(event):
         logger.debug(f'Inline query', extra=log_event(event.original_update))
 
         orig_text = event.text
-        glag_text = to_glag(orig_text)
+        glag_text = converter.convert('ukr', orig_text)
 
-        if not glag_text:
+        if not orig_text:
             return await event.answer([])
 
-        hint = 'надіслати транслітерацію в чат'
+        langs = converter.detect(orig_text)
+        options = []
         if len(orig_text) >= 255:
-            hint = '⚠️ можливо, текст занадто довгий!'
+            options.append(event.builder.article(
+                title='⚠️ можливо, текст занадто довгий!',
+                description='Якщо вам потрібно перетворити довший текст, напишіть боту в особисті.',
+                text='',
+            ))
 
-        return await event.answer([
-            event.builder.article(
-                title=f'{ellipsis_truncate(glag_text, 100)}',
-                description=f"{hint}",
+        lang_names = {
+            'ukr': "українська",
+            'isv': "меджусловјанскы",
+        }
+
+        for lang, lang_name in langs.items():
+            glag_text = converter.convert(lang, orig_text)
+            options.append(event.builder.article(
+                title=ellipsis_truncate(glag_text, 100),
+                description=lang_names[lang],
                 text=glag_text,
-            )
-        ], cache_time=CACHE_TIME)
+            ))
+
+        return await event.answer(options, cache_time=CACHE_TIME)
 
     return bot
 
@@ -74,9 +113,13 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 async def main():
     logger.info("Starting bot...")
 
-    session_root = Path('/var/run/sessions/')
+    var_run = Path('/var/run')
+    if not var_run.exists():
+        var_run = Path('.')
+
+    session_root = var_run / 'sessions'
     session_root.mkdir(exist_ok=True)
 
-    bot = make_bot(str(session_root / 'to_glag_bot'), [CYR_TABLE, ISV_TABLE])
+    bot = make_bot(str(session_root / 'to_glag_bot'), [UKR_TABLE, ISV_TABLE])
     await bot.start(bot_token=BOT_TOKEN)
     await bot.run_until_disconnected()
