5f2f33ba015ff856d5fb71e129a5ab63c1b35e1d
toriningen <toriningen.me@gmail.com>
2024-07-15 07:36:21 +0300

    wip

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import List
+from typing import List, Iterator
 
 from telethon import TelegramClient, events
 
@@ -20,11 +20,35 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def round_up(text: str, max_len: int) -> str:
+def ellipsis_truncate(text: str, max_len: int) -> str:
     if len(text) > max_len:
         return f'{text[:max_len - 3]}...'
 
     return text
+
+
+def split_long_text(text: str, max_len: int = 4096) -> Iterator[str]:
+    """Attempt to split text by line breaks, so that every chunk is not more than max_len.
+
+    If the split cannot be done by line breaks, then split by whitespace.
+
+    If cannot be done either, split by codepoints."""
+
+    while text:
+        closest_lf = text.rfind('\n', 0, max_len)
+        if closest_lf != -1:
+            yield text[:closest_lf]
+            text = text[closest_lf:]
+            continue
+
+        closest_whitespace = text.rfind(' ', 0, max_len)
+        if closest_whitespace != -1:
+            yield text[:closest_whitespace]
+            text = text[closest_whitespace:]
+            continue
+
+        yield text[:max_len]
+        text = text[max_len:]
 
 
 def make_bot(session: str, tables: List[str]) -> TelegramClient:
@@ -38,7 +62,11 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
 
     @bot.on(events.NewMessage(incoming=True))
     async def on_new_message(event):
-        await event.reply(to_glag(event.raw_text))
+        orig_text = event.raw_text
+        glag_text = to_glag(orig_text)
+
+        for chunk in split_long_text(glag_text):
+            await event.reply(chunk)
 
     @bot.on(events.InlineQuery())
     async def inline_handler(event):
@@ -48,22 +76,13 @@ def make_bot(session: str, tables: List[str]) -> TelegramClient:
         if not glag_text:
             return await event.answer([])
 
-        if len(glag_text) > 4096:
-            return await event.answer([
-                event.builder.article(
-                    title="Текст занадто довгий!",
-                    description="Обмеження - 4096 знаків",
-                    text='',
-                )
-            ])
-
         hint = ''
         if len(orig_text) == 255:
             hint = '(⚠️ можливо, текст занадто довгий - обмеження в 270 знаків) '
 
         return await event.answer([
             event.builder.article(
-                title=f'({len(orig_text)} | {len(glag_text)}) {round_up(glag_text, 100)}',
+                title=f'({len(orig_text)} | {len(glag_text)}) {ellipsis_truncate(glag_text, 100)}',
                 description=f"{hint}відправити транслітерацію в чат",
                 text=glag_text,
             )
